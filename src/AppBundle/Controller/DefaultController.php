059cd469b87d3eb61dbd63a9cfa070001b565289
Mark Smith <mark1979smith@googlemail.com>
2017-09-18 06:16:35 +0100

    Redis Cache GET and POST

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,9 +15,20 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         $returnedData = [];
+        $returnedData['method'] = $request->getRealMethod();
+        $returnedData['storage'] = $request->get('storage');
+        $returnedData['what'] = $request->get('what');
         switch ($request->getRealMethod()) {
             case $request::METHOD_GET: // Used for basic read requests to the server
+                if ($request->get('storage') == 'redis') {
+                    /** @var \Symfony\Component\Cache\Adapter\RedisAdapter $cache */
+                    $cache = $this->get('app.redis')->get();
 
+                    $returnedData['result'] = $cache->hasItem($request->get('what'));
+                    if ($returnedData['result']) {
+                        $returnedData['contents'] = $cache->getItem($request->get('what'))->get();
+                    }
+                }
                 break;
 
             case $request::METHOD_PUT: // Used to modify an existing object on the server
@@ -25,7 +36,14 @@ class DefaultController extends Controller
                 break;
 
             case $request::METHOD_POST: // Used to create a new object on the server
+                if ($request->get('storage') == 'redis') {
+                    /** @var \Symfony\Component\Cache\Adapter\RedisAdapter $cache */
+                    $cache = $this->get('app.redis')->get();
+                    $item = $cache->getItem($request->get('what'));
+                    $item->set((array) unserialize(base64_decode($request->get('data'))));
 
+                    $returnedData['result'] = $cache->save($item);
+                }
                 break;
 
             case $request::METHOD_DELETE: // Used to remove an object on the server
