7d787ad4bc791c2e9eb34b343f6c10912e42ab50
Mark Smith <mark1979smith@googlemail.com>
2017-11-14 13:00:14 +0000

    Corrected docblock

--- src/AppBundle/Source/S3.php
+++ src/AppBundle/Source/S3.php
@@ -23,7 +23,7 @@ class S3 extends SourceAbstract implements SourceInterface
 
     public function get()
     {
-        /** @var \AppBundle\Utils\S3Client $s3Service */
+        /** @var \AppBundle\Utils\AwsS3Client $s3Service */
         $s3Service = $this->container->get('app.aws.s3');
         $s3Client = $s3Service->get();
 
